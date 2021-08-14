1d60976dd014f139f0683a083cf720eee99a6160
ovenbakedgoods <ovenbakedgoods88@gmail.com>
2021-08-14 13:28:10 -0500

    Finished custom hook, custom message display success

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -14,8 +15,10 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+
+  const [values, showSuccessMessage, handleChanges, handleSubmit] = useForm(initialValue)
+  //const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  /*const [values, setValues] = useState(initialValue);
 
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -24,7 +27,7 @@ const CheckoutForm = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setShowSuccessMessage(true);
-  };
+  };*/
 
   return (
     <>
